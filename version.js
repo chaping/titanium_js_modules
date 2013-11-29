d049207183151ebd282d5a972105dc7d247f4f12
chaping <hcp0209@gmail.com>
2013-11-30 00:52:50 +0800

    update version.js

--- version.js
+++ version.js
@@ -1,51 +1,85 @@
 /**
 **********************************************
 *版本管理模块
+* 
+* var version = require('/lib/version');
+* version.check(url,opt);
+* 
+* url必须返回一个json数据，其键值包括:
+* {
+* 	version:版本号,
+*   url:新版本下载的url
+*   msg:新版本的描述
+* }
+* 
+* 
+* 1、支持设置检测的间隔时间
+* 2、当前的版本号的确定以tiapp.xml中的为准
+* 
+* 
+* 
+* 
 ***********************************************
 */
 var currentVersion = Ti.App.version; //当前版本
-var latestVersion = '';//最新版本
-var msg = '当前有一个新版本，赶快去下载升级吧！';
-var downUrl = '';
+var defaultMsg = '当前有一个新版本，赶快去下载升级吧！';
+var lastCheckTime = parseInt(Ti.App.Properties.getString('lastCheckTime'));//上次检测的时间
+lastCheckTime = lastCheckTime ? lastCheckTime : 0;
+var frequency = 1000*60*60*24;//检测频率
 
-function notify(){//通知有新的版本
+function notify(version,url,msg){//通知有新的版本
 	 var dialog = Ti.UI.createAlertDialog({
 		cancel: 1,
 		buttonNames: ['立即下载', '暂不升级'],
 		message: msg,
-		title: '发现新版本' + latestVersion
+		title: '发现新版本' + version
 	 });
 	 dialog.addEventListener('click', function(e){
 		if (e.index === e.source.cancel){
 			Ti.API.info('已取消');
 		} else if(e.index == 0){
-			if(downUrl) Ti.Platform.openUrl(downUrl);  		
+			if(url) Ti.Platform.openURL(url);  		
 		}
 	 });
 	 dialog.show();
 }
 
-function init(version_url,opt){
+function check(version_url,opt){
 	if(!version_url) return;
+	var opt = opt || {};
+	if(typeof opt.frequency == 'undefined' || !parseInt(opt.frequency)) opt.frequency = frequency;
+	var now = new Date;
+	if(now.getTime()-lastCheckTime < opt.frequency){
+		Ti.API.info('未到检测时间');
+		return;
+	}
 	try{
 		var http = require('/lib/http');
 	}catch(e){
 		////////
 	}
 	if(!http) return;
-	http.getJSON(version_url,{},function(data){
+	http.getJSON(version_url,{},function(data){//url必须返回一个json数据，此数据包含version,url,msg三个键
 		if(!data.version || !data.url) return;
-		latestVersion = data.version;
-		downUrl = data.url;
-		if(data.msg) msg = data.msg;
-		if(currentVersion < +version){
-			notify();
+		lastCheckTime = now.getTime();
+		Ti.App.Properties.setString('lastCheckTime',lastCheckTime);
+		var latestVersion = data.version;
+		var downUrl = data.url;
+		var msg = data.msg;
+		if(!msg) msg = defaultMsg;
+		if(currentVersion < + latestVersion){
+			notify(latestVersion,downUrl,msg);
 		}
 	},function(){
 	
 	});
 }
 
+if(OS_IOS){
+	Ti.App.addEventListener('resumed',function(e){
+		check();
+	});
+}
 
 /***************************************************/
-exports.init = init;
+exports.check = check;
